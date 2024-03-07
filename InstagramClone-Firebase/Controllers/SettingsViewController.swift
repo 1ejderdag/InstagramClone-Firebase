5fcb26c359fd1c7f0cd363624da541f8ad6723d9
Ejder Dag <ejdrdag@gmail.com>
2024-03-08 02:21:41 +0300

    creating users

---
+++ InstagramClone-Firebase/Controllers/SettingsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.swift
+//  InstagramClone-Firebase
+//
+//  Created by Ejder Dağ on 5.03.2024.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func logOutClicked(_ sender: UIButton) {
+        
+    }
+    
+
+
+}
